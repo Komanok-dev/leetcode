1050bb0170ac5c14a8b91472aec103699f1cd9c0
Игорь Терских <s.komanok@gmail.com>
2023-08-27 20:18:59 +0100

    files update

--- median-of-two-sorted-arrays.py
+++ median-of-two-sorted-arrays.py
@@ -1,7 +1,7 @@
 class Solution:
     # Time Complexity O(n+n)
     # Space Complexity O(1)
-    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+    def findMedianSortedArrays2(self, nums1: list[int], nums2: list[int]) -> float:
         i = len(nums1) - 1
         j = len(nums2) - 1
         last = len(nums1) + len(nums2) - 1
@@ -24,3 +24,4 @@ class Solution:
             return nums1[len(nums1) // 2]
         else:
             return (nums1[len(nums1) // 2 - 1] + nums1[len(nums1) // 2]) / 2
+
