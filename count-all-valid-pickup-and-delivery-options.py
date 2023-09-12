263955a7a5e670fac5b334bc26af9bc519deb3bd
Игорь Терских <s.komanok@gmail.com>
2023-09-12 12:00:59 +0100

    count all valid pickup and delivery options

--- count-all-valid-pickup-and-delivery-options.py
+++ count-all-valid-pickup-and-delivery-options.py
@@ -2,6 +2,6 @@ class Solution:
     def countOrders(self, n: int) -> int:
         MOD = 10 ** 9 + 7
         result = 1
-        for i in reversed(range(2, 2*n+1, 2)):
-            result = result * i * (i-1) // 2 % MOD
+        for i in range(1, n+1):
+            result = result * i * (2*i-1) % MOD
         return result
