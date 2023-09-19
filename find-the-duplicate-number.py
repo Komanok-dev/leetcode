c0ee5d67ae63a06a19970acd84ee109d418dc9c2
Игорь Терских <s.komanok@gmail.com>
2023-09-19 16:38:52 +0100

    find the duplicate number

--- find-the-duplicate-number.py
+++ find-the-duplicate-number.py
@@ -5,10 +5,9 @@ class Solution:
             if nums[i] == nums[i-1]:
                 return nums[i]
 
-
-
-# nums = [1,3,4,2,2]  # 2
-nums = [3,1,3,4,2]  # 3
-
-a = Solution()
-print(a.findDuplicate(nums))
+    def findDuplicate2(self, nums: list[int]) -> int:
+        hash = {}
+        for num in nums:
+            if hash.get(num, None) is not None:
+                return num
+            hash[num] = 1
