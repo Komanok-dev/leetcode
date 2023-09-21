07bed436286a567ecb3f77531eeed398c2f46cb4
Игорь Терских <s.komanok@gmail.com>
2023-09-21 22:19:33 +0100

    minimum operations to reduce x to zero

--- minimum-operations-to-reduce-x-to-zero.py
+++ minimum-operations-to-reduce-x-to-zero.py
@@ -1,9 +1,18 @@
 class Solution:
     def minOperations(self, nums: list[int], x: int) -> int:
-        pass
-
-
-
+        curr = sum(nums)
+        if curr < x:
+            return -1
+        left = 0
+        result = float('inf')
+        for right in range(len(nums)):
+            curr -= nums[right]
+            while curr < x and left <= right:
+                curr += nums[left]
+                left += 1
+            if curr == x:
+                result = min(left + len(nums) - right - 1, result)
+        return result if result != float('inf') else -1
 
     def minOperations2(self, nums: list[int], x: int) -> int:
         target = sum(nums) - x
