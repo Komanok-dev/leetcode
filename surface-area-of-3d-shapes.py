1f92c46ef149e1e94fc77d21e4595e2ed2bc82a8
Игорь Терских <s.komanok@gmail.com>
2023-09-30 13:35:55 +0100

    surface area of 3d shapes

---
+++ surface-area-of-3d-shapes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def surfaceArea(self, grid: list[list[int]]) -> int:
+        result = 0
+        for i in range(len(grid)):
+            for j in range(len(grid)):
+                if grid[i][j]:
+                    result += 2 + grid[i][j] * 4
+                if i:
+                    result -= min(grid[i][j], grid[i-1][j]) * 2
+                if j:
+                    result -= min(grid[i][j], grid[i][j-1]) * 2
+        return result
