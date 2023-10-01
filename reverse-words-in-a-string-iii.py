9972cbea141c32bb56d97e46b4066b9463aee758
Игорь Терских <s.komanok@gmail.com>
2023-10-01 13:54:56 +0100

    reverse words in a string ii

--- reverse-words-in-a-string-iii.py
+++ reverse-words-in-a-string-iii.py
@@ -1,9 +1,3 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         return ' '.join(reversed(s[::-1].split()))
-
-
-s = "Let's take LeetCode contest"
-# "s'teL ekat edoCteeL tsetnoc
-A = Solution()
-print(A.reverseWords(s))
