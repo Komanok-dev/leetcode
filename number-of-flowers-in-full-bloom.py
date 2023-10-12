672d1b8d19f5dc53853fa160e6003297b04b1edd
Игорь Терских <s.komanok@gmail.com>
2023-10-12 18:27:31 +0100

    number of flowers in full bloom

--- number-of-flowers-in-full-bloom.py
+++ number-of-flowers-in-full-bloom.py
@@ -1,3 +1,12 @@
+import bisect
+
+
 class Solution:
     def fullBloomFlowers(self, flowers: list[list[int]], people: list[int]) -> list[int]:
-        pass
+        starts, ends = [], []
+        for s, e in flowers:
+            starts.append(s)
+            ends.append(e+1)
+        starts.sort()
+        ends.sort()
+        return [bisect.bisect_right(starts, t)-bisect.bisect_right(ends, t) for t in people]
