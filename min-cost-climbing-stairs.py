1f258af805a214c3c0c3405b6f4d5e8cd6e69b8b
Игорь Терских <s.komanok@gmail.com>
2023-10-14 00:15:58 +0100

    min cost climbing stairs

--- min-cost-climbing-stairs.py
+++ min-cost-climbing-stairs.py
@@ -1,3 +1,8 @@
 class Solution:
     def minCostClimbingStairs(self, cost: list[int]) -> int:
-        pass
+        first, second = 0, 0
+        for i in range(2, len(cost)+1):
+            temp = first
+            first = min(first + cost[i-1], second + cost[i-2])
+            second = temp
+        return first
