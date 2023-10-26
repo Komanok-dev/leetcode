ef2f0d69cf60ba4263fb3f2a90b510c2ea2727c8
Игорь Терских <s.komanok@gmail.com>
2023-10-26 10:40:12 +0100

    contains duplicate

--- contains-duplicate.py
+++ contains-duplicate.py
@@ -1,5 +1,13 @@
 class Solution:
-    def containsDuplicate(self, nums: list[int]) -> bool:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hash = set()
+        for num in nums:
+            if num in hash:
+                return True
+            hash.add(num)
+        return False
+
+    def containsDuplicate2(self, nums: List[int]) -> bool:
         hash = {}
         for num in nums:
             if hash.get(num):
@@ -7,5 +15,12 @@ class Solution:
             else:
                 hash[num] = 1
 
-    def containsDuplicate2(self, nums: list[int]) -> bool:
+    def containsDuplicate3(self, nums: List[int]) -> bool:
         return False if len(set(nums)) == len(nums) else True
+
+    def containsDuplicate4(self, nums: List[int]) -> bool:
+        nums.sort()
+        for num in range(len(nums) - 1):
+            if nums[num] == nums[num+1]:
+                return True
+        return False
