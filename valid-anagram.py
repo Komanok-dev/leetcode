1e402f99123b19f9a53287c9a0579f573393d125
Игорь Терских <s.komanok@gmail.com>
2023-10-26 10:26:34 +0100

    valid anagram

--- valid-anagram.py
+++ valid-anagram.py
@@ -1,5 +1,23 @@
+from collections import Counter
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        hashS, hashT = {}, {}
+        for i in range(len(s)):
+            hashS[s[i]] = hashS.get(s[i], 0) + 1
+            hashT[t[i]] = hashT.get(t[i], 0) + 1
+        for key in hashS:
+            if hashS[key] != hashT.get(key, 0):
+                return False
+        return True
+
+    def isAnagram2(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+
+    def isAnagram3(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
         hash = {}
@@ -9,3 +27,6 @@ class Solution:
             if t.count(k) < v:
                 return False
         return True
+
+    def isAnagram4(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
