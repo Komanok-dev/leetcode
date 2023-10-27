e000edbccd81e2129a548d195559bd01098ef0a1
Игорь Терских <s.komanok@gmail.com>
2023-10-27 14:00:47 +0100

    files update

--- climbing-stairs.py
+++ climbing-stairs.py
@@ -1,13 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        first = 0
-        second = 1
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        for _ in range(1, n+1):
-            third = first + second
-            first = second
-            second = third
-        return second
+        if n < 4:
+            return n
+        one, two = 1, 1
+        for _ in range(n-1):
+            temp = one
+            one = one + two
+            two = temp
+        return one
