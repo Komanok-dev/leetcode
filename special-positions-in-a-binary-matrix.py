3896546ba1f9dcf76e4d157c10f5e2e37523d391
Komanok-dev <s.komanok@gmail.com>
2023-12-13 15:38:20 +0000

    special positions in a binary matrix

--- special-positions-in-a-binary-matrix.py
+++ special-positions-in-a-binary-matrix.py
@@ -1,22 +1,14 @@
 class Solution:
     def numSpecial(self, mat: list[list[int]]) -> int:
-        rows, cols = [0]*len(mat), [0]*len(mat[0])
+        count = 0
+        rows, cols = [0] * len(mat), [0] * len(mat[0])
         for i in range(len(rows)):
             for j in range(len(cols)):
                 if mat[i][j]:
                     rows[i] += 1
                     cols[j] += 1
-        print(rows)
-        print(cols)
-        
-        result = 0
         for i in range(len(rows)):
             for j in range(len(cols)):
                 if mat[i][j] == rows[i] == cols[j] == 1:
-                    result += 1
-        return result
-
-
-a = Solution()
-mat = [[1,0,0],[0,0,1],[1,0,0]]
-print(a.numSpecial(mat))
+                    count += 1
+        return count
