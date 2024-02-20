6afc61ee3aa2d89db4dbf7d8d65cd446d503247e
Komanok-dev <s.komanok@gmail.com>
2024-02-20 09:16:01 +0000

    missing number

--- missing-number.py
+++ missing-number.py
@@ -1,8 +1,3 @@
 class Solution:
     def missingNumber(self, nums: list[int]) -> int:
         return int((len(nums)*(len(nums)+1)/2) - sum(nums))
-
-
-nums = [9,6,4,2,3,5,7,0,1]
-a = Solution()
-print(a.missingNumber(nums))
